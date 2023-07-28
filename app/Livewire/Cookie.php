3e82be07ca8e38bb8213bad590374aa90a99b756
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-07-28 19:44:17 +0000

    Add cookies component

---
+++ app/Livewire/Cookie.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Cookie extends Component
+{
+    private array $messages = [  
+        'Time may fly by. But Memories don\'t.',  
+        'Nothing in the world is accomplished without passion.',  
+        'The project on your mind will soon gain momentum.',  
+        'A dream you have will come true.',  
+        'A journey of a thousand miles begins with a single step.',  
+    ];
+
+    // Message to display to the visitor
+    public string $message;
+
+    public function rotate()
+    {
+        $this->message = Arr::random($this->messages);
+    }
+
+    public function mount()  
+    {  
+        $this->rotate();  
+    }
+    
+    public function render()
+    {
+        return view('livewire.cookie');
+    }
+}
---
+++ resources/views/livewire/cookie.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1>{{ $message }}</h1>  
+
+    <button wire:click="rotate">  
+        Surprise Me!    
+    </button>
+</div>
