23e170f56765d75c75d27fde76c35d7ad310195e
khouloud HADDAD AMAMOU <khouloud.amamou@azurreo.com>
2023-07-28 20:54:04 +0100

    Fix error

--- app/Livewire/Cookie.php
+++ app/Livewire/Cookie.php
@@ -3,15 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Arr;
 
 class Cookie extends Component
 {
-    private array $messages = [  
-        'Time may fly by. But Memories don\'t.',  
-        'Nothing in the world is accomplished without passion.',  
-        'The project on your mind will soon gain momentum.',  
-        'A dream you have will come true.',  
-        'A journey of a thousand miles begins with a single step.',  
+    private array $messages = [
+        'Time may fly by. But Memories don\'t.',
+        'Nothing in the world is accomplished without passion.',
+        'The project on your mind will soon gain momentum.',
+        'A dream you have will come true.',
+        'A journey of a thousand miles begins with a single step.',
     ];
 
     // Message to display to the visitor
@@ -22,11 +23,11 @@ class Cookie extends Component
         $this->message = Arr::random($this->messages);
     }
 
-    public function mount()  
-    {  
-        $this->rotate();  
+    public function mount()
+    {
+        $this->rotate();
     }
-    
+
     public function render()
     {
         return view('livewire.cookie');
